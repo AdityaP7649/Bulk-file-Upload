cd99eb6ea7135ea432861d191cddfe81afd5c87f
Aditya Patil <adityapatil7649@gmail.com>
2024-04-03 13:39:29 +0530

    added functionalty to upload files to S3 bucket

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
+require("dotenv").config()
 const express = require("express");
 const multer = require("multer");
-const uuid = require("uuid").v4;
+const { s3Uploadv3 } = require("./s3service");
+
 const app = express();
-
-//Single file upload
-// const upload = multer({ dest: "uploads/" });
-// app.post("/upload", upload.single("file"), (req, res) => {
-//    res.json({ status: "success" });
-// });
-
-
-//Multiple file Upload
-// const upload = multer({ dest: "uploads/" });
-// app.post("/upload", upload.array("file", 100), (req, res) => {
-//     console.log(req.files);
-//     res.json({ status: "success" });
-// });
-
-
-//custom Name
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads");
-    },
-    filename: (req, file, cb) => {
-        const { originalname } = file;
-        cb(null, `${uuid()}-${originalname}`);
-    },
-});
-
+const storage = multer.memoryStorage()
 
 //File Filtering
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.split("/")[0] === "application") {
-        cb(null, true);
-    } else {
-        cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false); 
-    }
+  if (file.mimetype.split("/")[0] === "application") {
+    cb(null, true);
+  } else {
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
+  }
 };
 
-const upload = multer({ storage, fileFilter, limits: { fileSize: 3000000, files: 100 } });
-app.post("/upload", upload.array("file"), (req, res) => {
-    res.json({ status: "success" });
+//Setting file size limit and number of files to upload at once
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 3000000, files: 100 }
 });
 
 
+app.post("/upload", upload.array("file"), async (req, res) => {
+  try {
+    const results = await s3Uploadv3(req.files)
+    console.log(results);
+    return res.json({ status: "success" });
+  } catch (err) {
+    console.log(err);
+  };
+});
+
 //Error Handling
 app.use((error, req, res, next) => {
-    if (error instanceof multer.MulterError) {
-      if (error.code === "LIMIT_FILE_SIZE") {
-        return res.status(400).json({
-          message: "File is too large",
-        });
-      }
-  
-      if (error.code === "LIMIT_FILE_COUNT") {
-        return res.status(400).json({
-          message: "File limit reached",
-        });
-      }
-  
-      if (error.code === "LIMIT_UNEXPECTED_FILE") {
-        return res.status(400).json({
-          message: "File must be in pdf format",
-        });
-      }
+  if (error instanceof multer.MulterError) {
+    if (error.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).json({
+        message: "File is too large",
+      });
     }
-  });
+
+    if (error.code === "LIMIT_FILE_COUNT") {
+      return res.status(400).json({
+        message: "File limit reached",
+      });
+    }
+
+    if (error.code === "LIMIT_UNEXPECTED_FILE") {
+      return res.status(400).json({
+        message: "File must be in pdf format",
+      });
+    }
+  }
+});
 
 
 app.listen(4000, () => console.log("listening on port 4000"));
