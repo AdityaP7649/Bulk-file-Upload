40184e2a3a6d2b04e869756bcb24a151317b7544
Aditya Patil <adityapatil7649@gmail.com>
2024-04-05 21:05:04 +0530

    Changes

--- s3service.js
+++ s3service.js
@@ -2,7 +2,7 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const uuid = require("uuid").v4;
 
 exports.s3Uploadv3 = async (files) =>{
-    const s3cilent = new S3Client()
+    const s3client = new S3Client()
     const params = files.map(file => {
         return {
             Bucket: process.env.AWS_BUCKET_NAME,
